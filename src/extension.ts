38f14664aa8a8bc170490e05c758d989adee9378
Danil Beltyukov <beltyukovdanil@gmail.com>
2020-05-24 02:11:53 +0300

    fix everything

--- src/extension.ts
+++ src/extension.ts
@@ -1,50 +1,49 @@
-import * as vscode from 'vscode';
+import * as vscode from "vscode";
 
 export function activate(context: vscode.ExtensionContext) {
-	console.log("code-finder loaded");
+  console.log("code-finder loaded");
 
-	let provider = vscode.languages.registerCompletionItemProvider("plaintext", {
+  let provider = vscode.languages.registerCompletionItemProvider("plaintext", {
     provideCompletionItems(
       document: vscode.TextDocument,
       position: vscode.Position,
       token: vscode.CancellationToken,
       context: vscode.CompletionContext
     ) {
-    	const imports = [
-        	["numpy", "np"],
-        	["pandas", "pd"],
-        	["matplotlip.pyplot", "plt"],
-    	];
+      const imports = [
+        ["numpy", "np"],
+        ["pandas", "pd"],
+        ["matplotlip.pyplot", "plt"],
+        ["requests", "r"],
+      ];
 
-    	const completions: vscode.CompletionItem[] = [];
+      const completions: vscode.CompletionItem[] = [];
 
-    	// NE RABOTAYET
-    	imports.forEach(([name, alias]) => {
-        	const comp = new vscode.CompletionItem(`import`);
-        	comp.insertText = `import ${name} as ${alias}`;
-        	completions.push(comp);
-    	});
+      imports.forEach(([name, alias]) => {
+        const comp = new vscode.CompletionItem(`import ${name} as ${alias}`);
+        completions.push(comp);
+      });
 
-		const commandCompletion = new vscode.CompletionItem("new");
-		commandCompletion.kind = vscode.CompletionItemKind.Keyword;
-		commandCompletion.insertText = "new ";
-		commandCompletion.command = {
-			command: "editor.action.triggerSuggest",
-			title: "Re-trigger completions...",
-		};
-
-		return [commandCompletion, ...completions];
+      return completions;
     },
-	});
+  });
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	let disposable = vscode.commands.registerCommand('code-finder.codefinder', () => {
-		context.subscriptions.push(provider);
-	});
+  // Register autocomplete provider
+  context.subscriptions.push(provider);
 
-	context.subscriptions.push(disposable);
+  // The command has been defined in the package.json file
+  // Now provide the implementation of the command with registerCommand
+  // The commandId parameter must match the command field in package.json
+  let disposable = vscode.commands.registerCommand("code-finder.search", () => {
+    // context.subscriptions.push(provider);
+    vscode.window
+      .showInputBox({ prompt: "Enter query" })
+      .then((query) =>
+        query ? vscode.window.showInformationMessage(query) : null
+      );
+  });
+
+  context.subscriptions.push(disposable);
 }
 
 // this method is called when your extension is deactivated
