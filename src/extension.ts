97857d7f968ecc1619f9461490b1413ca57a616e
Vladislav <vlad@grokhotov.ru>
2020-05-24 20:11:11 +0300

    added all logic of github search

--- src/extension.ts
+++ src/extension.ts
@@ -1,10 +1,12 @@
 import * as vscode from "vscode";
 import { AutoCompleteProvider } from "./autocomple";
 import { Search } from "./search";
+//import { getSuggestions } from './github-search';
 
 export function activate(context: vscode.ExtensionContext) {
   context.subscriptions.push(AutoCompleteProvider, Search);
   console.log("code-finder loaded 🚀");
+  //getSuggestions("import", "python").then(result => console.log(result));
 }
 
 export function deactivate() {}
--- src/github-search.ts
+++ src/github-search.ts
@@ -1,54 +1,99 @@
-import * as vscode from 'vscode';
-import * as fs from 'fs';
+import * as vscode from "vscode";
+import * as fs from "fs";
+import { Octokit } from "@octokit/rest";
 
-const END_POINT: String = "https://api.github.com/"
-const SEARCH_REPOS_METHOD: String = "search/repositories"
-const SEARCH_CODE_METHOD: String = "search/code"
+const TOKEN: string = "";
 
-const TOKEN: String = ""
+const octokit = new Octokit({
+  auth: TOKEN || null,
+  userAgent: "ange1of/code-finder v0.0.1"
+})
 
-export function getSuggestions(construction: String, language: string) {
+// type GithubRepo = {
+//    fullName: string;
+// } // { "fullName", "fdsm;kfn", "fmdf"}
 
-}
-
-async function makeGithubCall(url: String) {
-    const response = await fetch(url, {
-      headers: {
-        authorization: `token ${TOKEN}`
-      }
-    }).then(function(response: Response) {
-      if (response.headers.get("X-Ratelimit-Remaining") == "0") {
-        let diff = Number(response.headers.get("X-Ratelimit-Reset"))*1000 - Date.now().valueOf();
-        function delay(ms: number) {
-          return new Promise( resolve => setTimeout(resolve, ms) );
-        }
-        delay(diff)
-      }
-    }).then(
-      async (result: any) => await result.json()
-    )
-}
-
-function getReposForLanguage(language: string) {
-    const filename = `repo_${language}.json`
-    if (!fs.existsSync(filename)) {
-      
-    }
-}
-
-
-// function getWebhook() {
-//     let promise = fetch('/api/webhook')
-//     .then(
-//         result => result.json()
-//     ).then(
-//         result => {
-//             if (result.webhook) {
-//                 $webhookUrl = result.webhook;
-//             }
-//             else {
-//                 return Promise.reject();
-//             }
-//         }
-//     );
+// type GithubFileCode = {
+//   fileUrl: string;
+//   codes: string[];
+//   fileName: string;
 // }
+
+// type ReposForLanguage = {
+//   repos: GithubRepo
+// }
+
+export async function getSuggestions(construction: string, language: string) {
+  const constructions = (await getLocalConstructions(language))
+    .filter( x => x.includes(construction) );
+  if (!Array.isArray(constructions) || !constructions.length){
+    return loadConstructions(construction, language);
+  } else {
+    return constructions;
+  }
+}
+
+async function getLocalConstructions(language: string): Promise<string[]> {
+  if (fs.existsSync(`DB_KEK_${language}`)){
+    const data = fs.readFileSync(`DB_KEK_${language}`, {encoding: "utf-8"}).toString();
+    return JSON.parse(data);
+  } 
+  
+  return [];
+}
+
+async function loadConstructions(construction: string, language: string): Promise<string[]> {
+  const repos = await getReposByLanguage(language);
+  const constructions: string[] = [];
+  for (const repo in repos) {
+    const codes = await getConstructionUrlsFromRepo(construction, language, repo);//урлы конструкций
+    for (const contentURL in codes){
+      const content = await getCodesFromUrl(contentURL);
+      const contentToSave = Buffer.from(content, 'base64').toString('binary');
+      constructions.push(contentToSave);
+    }
+  }
+  return constructions;
+}
+
+async function getReposByLanguage(language: string): Promise<string[]> {
+  const filename = `repo_${language}.json`;
+  if (!fs.existsSync(filename)) {
+    await savePopularReposByLanguage(language);
+  }
+  const data = fs.readFileSync(filename, {encoding: "utf-8"}).toString();
+  return JSON.parse(data);
+}
+
+async function savePopularReposByLanguage(language: string): Promise<void> {
+    const repos = await octokit.search.repos({q: `language:${language}`, order: "desc", per_page: 10});
+    const repoList: string[] = [];
+
+    for (const repo of repos.data.items){
+        repoList.push(repo.full_name);
+    }
+
+    fs.writeFileSync(`repos_${language}.json`, JSON.stringify(repos));
+}
+
+async function getConstructionUrlsFromRepo(construction: string, language: string, repo: string): Promise<string[]> {
+  const codes = await octokit.search.code({q: `${construction} language:${language} repo:${repo}`});
+  const codeUrlsList: string[] = [];
+  
+  for (const code of codes.data.items) {
+    codeUrlsList.push(code.git_url);
+  }
+  return codeUrlsList;
+}
+
+async function getCodesFromUrl(url: string): Promise<string> {
+  // return fetch(url).then( (res: Response) => res.json())
+  // .then( (x: any) => x.content as string)
+  
+  let response = await fetch(url);
+  let json = await response.json() as Record<string, unknown>;
+  return json["content"] as string;
+
+}
+
+getSuggestions("import", "python").then(result => console.log(result));
