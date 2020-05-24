f57a603d8fded66a5f9e4db0aefe9f6d0b7e2fc5
Danil Beltyukov <beltyukovdanil@gmail.com>
2020-05-24 18:07:47 +0300

    remove hardcoded completions

--- src/autocomplete.ts
+++ src/autocomplete.ts
@@ -21,24 +21,9 @@ export const createCompletionsProvider = (context: vscode.ExtensionContext) => {
       token: vscode.CancellationToken,
       context: vscode.CompletionContext
     ) {
-      const imports = [
-        ["numpy", "np"],
-        ["pandas", "pd"],
-        ["matplotlip.pyplot", "plt"],
-        ["requests", "r"],
-      ];
-
-      const completions: vscode.CompletionItem[] = [];
-
-      imports.forEach(([name, alias]) => {
-        const comp = new vscode.CompletionItem(`import ${name} as ${alias}`);
-        completions.push(comp);
-      });
-
-      return [
-        ...completionsFromFile.map((x: string) => new vscode.CompletionItem(x)),
-        ...completions,
-      ];
+      return completionsFromFile.map(
+        (x: string) => new vscode.CompletionItem(x)
+      );
     },
   });
 };
