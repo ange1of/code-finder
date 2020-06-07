4f5e296090d50c6e00c7de646bf9f97b845b4a4a
Ivan Angelov <angelow.van@yandex.ru>
2020-06-08 01:23:38 +0300

    Add Issues search command

--- src/extension.ts
+++ src/extension.ts
@@ -1,12 +1,16 @@
 import vscode from "vscode";
 import { createCompletionsProvider } from "./autocomplete";
-import { Search } from "./search";
+import { Search, SearchIssues } from "./search";
 import { GithubSearch } from "./github-search";
 
 export function activate(context: vscode.ExtensionContext) {
   const githubSearch = new GithubSearch(context.extensionPath);
   const autoCompleteProvider = createCompletionsProvider(context, githubSearch);
-  context.subscriptions.push(autoCompleteProvider, Search(context));
+  context.subscriptions.push(
+    autoCompleteProvider, 
+    Search(context),
+    SearchIssues(context)
+  );
   console.log("code-finder loaded 🚀");
 }
 
--- src/search.ts
+++ src/search.ts
@@ -8,47 +8,59 @@ const jsdom = require("jsdom");
 const { JSDOM } = jsdom;
 
 export const Search = (context: vscode.ExtensionContext) => {
-
 	return vscode.commands.registerCommand(
 		"code-finder.search",
-		() => {
-			const editor = vscode.window.activeTextEditor;
-			const query = editor?.document.getText(editor.selection);
-			const language = editor?.document.languageId;
-			console.info(`Query: ${query}`);
-
-			if (!query) {
-				return;
-			}
-
-			const panel = vscode.window.createWebviewPanel(
-				'searchResults', // Identifies the type of the webview. Used internally
-				'Search Results', // Title of the panel displayed to the user
-				vscode.ViewColumn.One, // Editor column to show the new webview panel in.
-				{
-					localResourceRoots: [vscode.Uri.file(path.join(context.extensionPath, 'static'))]
-				} // Webview options. More on these later.
-			);
-
-			try {
-				let webviewManager = new WebviewManager(context.extensionPath, panel.webview);
-				panel.webview.html = '<h1 style="font-weight: lighter">Loading...</h1>';
-				webviewManager.renderQuery(query, language);
-			}
-			catch (ex) {
-				panel.webview.html = `<h2>Unable to display results<h2>\n
-				<h3>${ex}</h3>`;
-			}
-		}
+		() => handleQuery(WebviewManager.prototype.getCode, context)
 	);
 };
+
+export const SearchIssues = (context: vscode.ExtensionContext) => {
+	return vscode.commands.registerCommand(
+		"code-finder.searchIssues",
+		() => handleQuery(WebviewManager.prototype.getIssue, context)
+	);
+};
+
+function handleQuery(handler: Function, context: vscode.ExtensionContext): void {
+	const editor = vscode.window.activeTextEditor;
+	const query = editor?.document.getText(editor.selection);
+	const language = editor?.document.languageId;
+	console.info(`Query: ${query}`);
+
+	if (!query) {
+		return;
+	}
+
+	const panel = vscode.window.createWebviewPanel(
+		'searchResults', // Identifies the type of the webview. Used internally
+		'Search Results', // Title of the panel displayed to the user
+		vscode.ViewColumn.One, // Editor column to show the new webview panel in.
+		{
+			localResourceRoots: [vscode.Uri.file(path.join(context.extensionPath, 'static'))]
+		} // Webview options. More on these later.
+	);
+
+	try {
+		let webviewManager = new WebviewManager(context.extensionPath, panel.webview, handler);
+		panel.webview.html = '<h1 style="font-weight: lighter">Loading...</h1>';
+		webviewManager.renderQuery(query, language);
+	}
+	catch (ex) {
+		panel.webview.html = `<h2>Unable to display results<h2>\n
+		<h3>${ex}</h3>`;
+	}
+}
 
 export class WebviewManager {
 
 	private filePath: string;
 	private dom: any;
 
-	constructor(private extensionPath: string, private webviewObject: vscode.Webview) {
+	constructor(
+		private extensionPath: string, 
+		private webviewObject: vscode.Webview, 
+		private getContentFunc: Function
+	) {
 		this.filePath = path.resolve(this.extensionPath, 'static', 'index.html');
 		this.dom = new JSDOM(this.loadHtml());
 		this.updatePage();
@@ -59,9 +71,9 @@ export class WebviewManager {
 
 		language = language || 'python';
 
-		this.getContent(query, language)
+		this.getContentFunc.call(this, query, language)
 			.then(
-				contentBlockArray => {
+				(contentBlockArray: Array<IssueSuggestion | SearchSuggestion>) => {
 					let document = this.dom.window.document;
 					document.querySelector('#query > div').textContent += query;
 					this.updatePage();
@@ -100,29 +112,13 @@ export class WebviewManager {
 		this.webviewObject.html = this.dom.serialize();
 	}
 
-	private async getContent(query: string, language: string): Promise<Array<SearchSuggestion | IssueSuggestion>> {
-		// try {
-		// 	return (await new GithubSearch(this.extensionPath).getSearchSuggestions(query, language));
-		// } catch (err) {
-		// 	vscode.window.showErrorMessage(err.toString());
-		// 	return [];
-		// }
-
-		return [
-			new IssueSuggestion(
-				"https://api.github.com/repos/batterseapower/pinyin-toolkit/issues/132",
-				"https://api.github.com/repos/batterseapower/pinyin-toolkit",
-				"Line Number Indexes Beyond 20 Not Displayed",
-				"open",
-				"2009-07-12T20:10:41Z",
-				"2009-07-19T09:23:43Z",
-				null,
-				"Awesome body Awesome body Awesome body Awesome body Awesome body",
-				"Nick3C",
-				"https://api.github.com/users/Nick3C",
-				15
-			)
-		];
+	public async getCode(query: string, language: string): Promise<Array<SearchSuggestion>> {
+		try {
+			return (await new GithubSearch(this.extensionPath).getSearchSuggestions(query, language));
+		} catch (err) {
+			vscode.window.showErrorMessage(err.toString());
+			return [];
+		}
 
 		// return [
 		// 	new SearchSuggestion(
@@ -144,6 +140,31 @@ export class WebviewManager {
 		// 		'test-repo',
 		// 		'http://lolkek.ru/test_repo'
 		// 	),
+		// ];
+	}
+
+	public async getIssue(query: string, language: string): Promise<Array<IssueSuggestion>> {
+		try {
+			return (await new GithubSearch(this.extensionPath).getIssuesSuggestions(query, language));
+		} catch (err) {
+			vscode.window.showErrorMessage(err.toString());
+			return [];
+		}
+		
+		// return [
+		// 	new IssueSuggestion(
+		// 		"https://api.github.com/repos/batterseapower/pinyin-toolkit/issues/132",
+		// 		"https://api.github.com/repos/batterseapower/pinyin-toolkit",
+		// 		"Line Number Indexes Beyond 20 Not Displayed",
+		// 		"open",
+		// 		"2009-07-12T20:10:41Z",
+		// 		"2009-07-19T09:23:43Z",
+		// 		null,
+		// 		"Awesome body Awesome body Awesome body Awesome body Awesome body",
+		// 		"Nick3C",
+		// 		"https://api.github.com/users/Nick3C",
+		// 		15
+		// 	)
 		// ];
 	}
 }
