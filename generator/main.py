1727558ed047f839d5b620f33266b74a5b862455
Pepega <leeeloooda@gmail.com>
2020-06-07 17:13:38 +0300

    generate repos

--- generator/main.py
+++ generator/main.py
@@ -9,49 +9,20 @@ from tqdm import tqdm
 from ratelimit import limits, sleep_and_retry, RateLimitException
 from backoff import on_exception, expo
 
-
-LANGUAGE = "python"
 REPO_COUNT = 5
 CODE_COUNT = 10000
 GITHUB_RAW_URL = "https://raw.githubusercontent.com/{full_name}/master/{path}"
 USER_AGENT = "ange1of/code-finder v0.0.1"
 CONSTRUCTIONS = [
-    "import"
+    "import", "#include", "package"
 ]
-BAN_WORDS = ["def", "#"]
 
-def remove_comments(data):
-    base = io.StringIO(data)
-    tokgen = tokenize.generate_tokens(base.readline)
-    prev_toktype = token.INDENT
-    first_line = None
-    last_lineno = -1
-    last_col = 0
+COMPLETIONS_FILE_NAME = "resources/completions/completions_{language}.json"
+REPOS_FILE_NAME = "resources/repos/repos_{language}.json"
 
-    clean_file = io.StringIO()
-
-    for toktype, ttext, (slineno, scol), (elineno, ecol), ltext in tokgen:
-        if slineno > last_lineno:
-            last_col = 0
-        if scol > last_col:
-            clean_file.write(" " * (scol - last_col))
-        if toktype == token.STRING and prev_toktype == token.INDENT:
-            # Docstring
-            # mod.write("#--")
-            pass
-        elif toktype == tokenize.COMMENT:
-            # Comment
-            # mod.write("##\n")
-            pass
-        else:
-            clean_file.write(ttext)
-        prev_toktype = toktype
-        last_col = ecol
-        last_lineno = elineno
-
-    value = clean_file.getvalue()
-    clean_file.close()
-    return value
+LANGUAGES = [
+    "C", "C++", "C#", "F#", "Java", "Kotlin", "Javascript", "Swift", "Scala", "Haskell", "Python"
+]
 
 
 @sleep_and_retry
@@ -66,6 +37,42 @@ def get_content(full_name, path):
 
     return r.text
 
+def generate_completeions(g):
+    completions = set()
+
+    print(f"Parsing top {REPO_COUNT} repos")
+
+    for language in LANGUAGES:
+        try:
+            for repo in g.search_repositories(f"language:{language}", order="desc", sort="stars")[:REPO_COUNT]:
+                print(repo.full_name)
+                for construction in CONSTRUCTIONS:
+                    cursor = g.search_code(f"{construction} repo:{repo.full_name} language:{language}")
+                    count = min(cursor.totalCount, CODE_COUNT)
+                    print(count)
+                    for code in tqdm(cursor[:count], total=count):
+                        try:
+                            content = get_content(code.repository.full_name, code.path)
+                        except requests.RequestException as e:
+                            tqdm.write(f"error: {e} {code.repository.full_name}/{code.path}")
+                            continue
+                            
+                print()
+        except KeyboardInterrupt:
+            print("Saving state")
+            pass
+        
+        with open(COMPLETIONS_FILE_NAME.format(language=language), "w") as f:
+            f.write(json.dumps(list(completions)))
+
+def generate_repos(g):
+    for language in LANGUAGES:
+        repos = set()
+        for repo in g.search_repositories(f"language:{language}", order="desc", sort="stars")[:REPO_COUNT]:
+            repos.add(repo.full_name)
+        with open(REPOS_FILE_NAME.format(language=language), "w") as f:
+            f.write(json.dumps(list(repos)))
+
 
 if __name__ == "__main__":
     gh_token = os.environ.get("GITHUB_TOKEN")
@@ -75,33 +82,6 @@ if __name__ == "__main__":
 
     g = Github(gh_token)
 
-    completions = set()
+    generate_completeions(g)
+    # generate_repos(g)
 
-    print(f"Parsing top {REPO_COUNT} repos")
-
-    try:
-        for repo in g.search_repositories(f"language:{LANGUAGE}", order="desc", sort="stars")[:REPO_COUNT]:
-            print(repo.full_name)
-            for construction in CONSTRUCTIONS:
-                cursor = g.search_code(f"{construction} repo:{repo.full_name} language:{LANGUAGE}")
-                count = min(cursor.totalCount, CODE_COUNT)
-                print(count)
-                for code in tqdm(cursor[:count], total=count):
-                    try:
-                        content = get_content(code.repository.full_name, code.path)
-                    except requests.RequestException as e:
-                        tqdm.write(f"error: {e} {code.repository.full_name}/{code.path}")
-                        continue
-
-                    content = remove_comments(content)
-                    for line in content.splitlines():
-                        if not any([x in line for x in BAN_WORDS]) and line.strip():
-                            completions.add(line.strip())
-            print()
-
-    except KeyboardInterrupt:
-        print("Saving state")
-        pass
-    
-    with open("completions.json", "w") as f:
-        f.write(json.dumps(list(completions)))
